f79751a9c72de5a5fc71d15730925fea5a255664
AbdullahBağcı <47869289+ABDULLAHBAGCI@users.noreply.github.com>
2021-11-04 18:00:06 +0100

    andy's update files

--- assets/video.js
+++
@@ -1,8 +0,0 @@
-
-// common
-export const videoDesktopJpg = require('./img/bg01.jpg');
-export const videoMobileJpg = require('./img/bg01.jpg');
-
-
-export const videoMobileMp4 = require('./video/SolarMineX_mobile.webm');
-export const videoDesktopMp4 = require('./video/SolarMineX_desktop.webm');
